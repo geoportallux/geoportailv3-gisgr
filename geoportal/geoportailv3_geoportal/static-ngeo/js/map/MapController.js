f29c1d228287b1360aa4f545a1596f308e946eb1
Joe Mayer <lsb257@Mayers-Mac-mini.local>
2020-02-06 13:18:57 +0100

    Fix enable3DMode variable

--- geoportal/geoportailv3_geoportal/static-ngeo/js/map/MapController.js
+++ geoportal/geoportailv3_geoportal/static-ngeo/js/map/MapController.js
@@ -22,11 +22,6 @@ import {getTransform, transform} from 'ol/proj.js';
  */
 const exports = function(appStateManager, ngeoDebounce, enable3DMode) {
   var lurefToWebMercatorFn = getTransform('EPSG:2169', 'EPSG:3857');
-  /**
-   * @type {boolean}
-   * @export
-   */
-  this.enable3DMode = enable3DMode;
 
   /**
    * @type {boolean}
